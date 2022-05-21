e58dc64c3ccaa9adc667f3643c5ff2f1f4ccb638
Boakai <boakaidkamara@gmail.com>
2022-05-21 00:13:41 +0000

    add comment before invoking the function

--- counting_words.py
+++ counting_words.py
@@ -2,4 +2,6 @@ def count_words(text,word):
     word_apperance = text.count(word)
     print(word_apperance)
 
+#invove function
 count_words("I love coding because with it I am able to become innovative and impactful", "I")
+
